df44609e54776f22aa9d4941e33c71c7a67d92ae
oscar7845 <oc59@njit.edu>
2022-08-16 18:13:27 +0200

    Initial commit

---
+++ src/components/AppHeader.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Button, { SelectButton } from './Button';
+import styles from '../styles/modules/app.module.scss';
+import TodoModel from './TodoModel';
+import { updateFilterStatus } from '../slices/todoSlice';
+
+function AppHeader() {
+  const [modelOpen, setModelOpen] = useState(false);
+  const initialFilterStatus = useSelector((state) => state.todo.filterStatus);
+  const [filterStatus, setFilterStatus] = useState(initialFilterStatus);
+  const dispatch = useDispatch();
+
+  const updateFilter = (e) => {
+    setFilterStatus(e.target.value);
+    dispatch(updateFilterStatus(e.target.value));
+  };
+
+  return (
+    <div className={styles.appHeader}>
+      <Button variant="primary" onClick={() => setModelOpen(true)}>
+        Add Task
+      </Button>
+      <SelectButton
+        id="status"
+        onChange={(e) => updateFilter(e)}
+        value={filterStatus}
+      >
+        <option value="all">All</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">Completed</option>
+      </SelectButton>
+      <TodoModel type="add" modelOpen={modelOpen} setModelOpen={setModelOpen} />
+    </div>
+  );
+}
+
+export default AppHeader;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,97 @@
+import { motion } from 'framer-motion';
+import toast from 'react-hot-toast';
+import React, { useEffect, useState } from 'react';
+import { MdDelete, MdEdit } from 'react-icons/md';
+import { useDispatch } from 'react-redux';
+import { deleteTodo, updateTodo } from '../slices/todoSlice';
+import styles from '../styles/modules/todoItem.module.scss';
+import { getClasses } from '../utils/getClasses';
+import TodoModel from './TodoModel';
+import CheckButton from './CheckButton';
+
+const child = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+function TodoItem({ todo }) {
+  const dispatch = useDispatch();
+  const [checked, setChecked] = useState(false);
+  const [updateModelOpen, setUpdateModelOpen] = useState(false);
+
+  useEffect(() => {
+    if (todo.status === 'complete') {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+  }, [todo.status]);
+
+  const handleCheck = () => {
+    setChecked(!checked);
+    dispatch(
+      updateTodo({ ...todo, status: checked ? 'incomplete' : 'complete' })
+    );
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(todo.id));
+    toast.success('Todo Deleted Successfully');
+  };
+
+  const handleUpdate = () => {
+    setUpdateModelOpen(true);
+  };
+
+  return (
+    <>
+      <motion.div className={styles.item} variants={child}>
+        <div className={styles.todoDetails}>
+          <CheckButton checked={checked} handleCheck={handleCheck} />
+          <div className={styles.texts}>
+            <p
+              className={getClasses([
+                styles.todoText,
+                todo.status === 'complete' && styles['todoText--completed'],
+              ])}
+            >
+              {todo.title}
+            </p>
+            <p className={styles.time}>{todo.time}</p>
+          </div>
+        </div>
+        <div className={styles.todoActions}>
+          <div
+            className={styles.icon}
+            onClick={() => handleDelete()}
+            onKeyDown={() => handleDelete()}
+            tabIndex={0}
+            role="button"
+          >
+            <MdDelete />
+          </div>
+          <div
+            className={styles.icon}
+            onClick={() => handleUpdate()}
+            onKeyDown={() => handleUpdate()}
+            tabIndex={0}
+            role="button"
+          >
+            <MdEdit />
+          </div>
+        </div>
+      </motion.div>
+      <TodoModel
+        type="update"
+        modelOpen={updateModelOpen}
+        setModelOpen={setUpdateModelOpen}
+        todo={todo}
+      />
+    </>
+  );
+}
+
+export default TodoItem;
